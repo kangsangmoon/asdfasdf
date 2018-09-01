6c2702e538a0e70f9e4dddcd6d8269947dd15a87
kangsangmoon <sangmoonkang@naver.com>
2018-09-01 16:19:13 +0900

    sms

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,19 +24,20 @@ public class MainActivity extends AppCompatActivity implements Gota.OnRequestPer
         setContentView(R.layout.activity_main);
 
         new Gota.Builder(this)
-                .withPermissions(Manifest.permission.CAMERA,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.CALL_PHONE)
+                .withPermissions(Manifest.permission.CAMERA,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.SEND_SMS)
                 .requestId(1)
                 .setListener(this)
                 .check();
         final EditText phone_number = (EditText)findViewById(R.id.editText);
+        final EditText text = findViewById(R.id.editText2);
         Button button=(Button)findViewById(R.id.button);
 
        button.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone_number.getText().toString()));
-               if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
-                   startActivity(intent);
+               Intent intent = new Intent(Intent.ACTION_CALL, Uri.fromParts(text.getText().toString(), phone_number.getText().toString(),null));
+               if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                   SmsManager.getDefault().sendTextMessage(phone_number.getText().toString(), null, text.getText().toString(), null, null);
                }
            }
        });
